3e4565ef8d1425eef1a0db871da2f0d111dff4b6
aljapri <gamer2mohamad@gmail.com>
2024-06-03 18:50:57 +0300

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["flowbite.s3.amazonaws.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "flowbite.s3.amazonaws.com",
+        pathname: "**",
+      },
+    ],
   },
 };
 
